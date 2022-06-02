2321191ab98da344b8c8ad6696d14c75b99f5462
José Edston Azevedo <joseedston@gmail.com>
2022-06-02 14:36:25 -0300

    Remoção contato e sobre

--- recipes/views.py
+++ recipes/views.py
@@ -1,16 +1,5 @@
-from multiprocessing import context
-
-from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
-    return render(request, 'recipes/home.html', context={'name': 'José Edston',})
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
+    return render(request, 'recipes/home.html')
